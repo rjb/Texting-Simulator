d4cb083bc1107e1b738f9e127e75c4273856f166
Raffy Banks <raffybanks@gmail.com>
2016-09-07 22:13:13 -0700

    Initial commit. Working version

---
+++ README.md
@@ -0,0 +1,13 @@
+# Texting Simulator
+
+Simulates typing and back and forth text conversations.
+
+### type()
+
+Simply pass a string, element, and chpm and type() will simulate typing at the chpm rate.
+
+Determine your chpm at http://www.powertyping.com/mo/testmobile.html
+
+### convo()
+
+Pass string, element, wpm, and optional callback and convo() will simulate a back and forth conversation.
---
+++ simulator.js
@@ -0,0 +1,46 @@
+// Converts characters typed per minute to milliseconds
+function chpmToMilliseconds(chpm = 100) {
+  return (Math.random() * (1000 / (chpm / 60)));
+}
+
+// Types out any phrase to any html element at rate of x chpm
+function type(string, element, chpm) {
+  (function likeAHuman(i) {
+    if (string.length <= i++) { return element.innerHTML = string; }
+
+    var milliseconds = chpmToMilliseconds(chpm);
+    var currentString = string.substring(0, i)
+
+    element.innerHTML = currentString;
+
+    setTimeout(function() { likeAHuman(i); }, milliseconds);
+
+    console.log(milliseconds);
+    console.log(currentString)
+  })(0)
+}
+
+// Pause, type, print
+function convo(string, element, wpm, callback) {
+    var pause = 1250;
+    var rate = ((string.split(' ').length / (wpm / 60)) * 1000) * 0.65
+
+    setTimeout(function() { element.innerHTML = 'Typing...'; }, pause);
+
+    setTimeout(function() {
+      element.innerHTML = string;
+      callback && callback();
+    }, rate + pause);
+}
+
+type('I am running to the store. Need anything?', document.getElementById('mike'), 140)
+type('Having a great time down in Portland!', document.getElementById('elizabeth'), 325)
+type('Feeling grumpy :( Going to get into bed and putting on my stories.', document.getElementById('joy'), 245)
+
+convo("Hey. How's your trip going?", document.getElementById('dave'), 54, function() {
+  convo("Great. We just arrived in Paris!", document.getElementById('jack'), 38, function() {
+    convo("Nice! Don't forget to check out the Louvre", document.getElementById('dave'), 59, function() {
+      convo('Will do!', document.getElementById('jack'), 48)
+    })
+  });
+})
