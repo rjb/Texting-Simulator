69fa4d4935fe949e48da796dc6f663a32b370259
Raffy Banks <raffybanks@gmail.com>
2016-09-07 22:39:34 -0700

    Added optional parameter. Checks for rate and set texting speed to passed rate. E.g. 1x, 2x, 3x, etc

--- simulator.js
+++ simulator.js
@@ -5,7 +5,7 @@ function chpmToMilliseconds(chpm = 100) {
 
 // Returns words typed per minute in milliseconds
 function wpmToMilliseconds(string, wpm) {
-  return ((string.split(' ').length / (wpm / 60)) * 1000)
+  return ((string.split(' ').length / (wpm / 60)) * 1000);
 }
 
 // Types out any phrase to any html element at rate of x chpm
@@ -26,26 +26,27 @@ function type(string, element, chpm) {
 }
 
 // Pause, type, print
-function convo(string, element, wpm, callback) {
-    var pause = 1250;
-    var rate = wpmToMilliseconds(string, wpm);
+function convo(string, element, wpm, options, callback) {
+  var rate = options.rate || 1
+  var pause = 1000;
+  var milliseconds = wpmToMilliseconds(string, wpm) / rate;
 
-    setTimeout(function() { element.innerHTML = 'Typing...'; }, pause);
+  setTimeout(function() { element.innerHTML = 'Typing...'; }, pause);
 
-    setTimeout(function() {
-      element.innerHTML = string;
-      callback && callback();
-    }, rate + pause);
+  setTimeout(function() {
+    element.innerHTML = string;
+    callback && callback();
+  }, milliseconds + pause);
 }
 
 type('I am running to the store. Need anything?', document.getElementById('mike'), 140)
 type('Having a great time down in Portland!', document.getElementById('elizabeth'), 325)
 type('Feeling grumpy :( Going to get into bed and putting on my stories.', document.getElementById('joy'), 245)
 
-convo("Hey. How's your trip going?", document.getElementById('dave'), 54, function() {
-  convo("Great. We just arrived in Paris!", document.getElementById('jack'), 38, function() {
-    convo("Nice! Don't forget to check out the Louvre", document.getElementById('dave'), 59, function() {
-      convo('Will do!', document.getElementById('jack'), 48)
+convo("Hey. How's your trip going?", document.getElementById('dave'), 54, { rate: 2 }, function() {
+  convo("Great. We just arrived in Paris!", document.getElementById('jack'), 38, { rate: 4 }, function() {
+    convo("Nice! Don't forget to check out the Louvre", document.getElementById('dave'), 59, { rate: 3 }, function() {
+      convo('Will do!', document.getElementById('jack'), 48, {})
     })
   });
 })
